0bf57e05425b30a1fac229e45d4ead7b8f3e9472
believemanasseh <danasmufasa@gmail.com>
2020-05-30 01:23:53 +0100

    added new url patterns

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from shop.views import *
 
+app_name = 'shop'
 
 urlpatterns = [
-	path('', homepage, name='home'),
+	path('', HomepageView.as_view(), name='home'),
+	path('products/', ProductsView.as_view(), name='products'),
+	path('checkout/', CheckoutView.as_view(), name='checkout'),
 ]
