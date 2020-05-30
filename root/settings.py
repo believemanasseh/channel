f6100e886fb0d1338a0838d04e6d0f6f271301d7
believemanasseh <danasmufasa@gmail.com>
2020-05-30 01:19:30 +0100

    changed debug mode to False

--- root/settings.py
+++ root/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7x4sy#kqi^!wdwu2o)_43u$!^na%(%vud522*s@)@lfkb=k7+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'c-hannel.herokuapp.com']
 
