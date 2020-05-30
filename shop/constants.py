5ef791ae44a96e30ed2285e4b7743c7259837641
believemanasseh <danasmufasa@gmail.com>
2020-05-30 02:05:24 +0100

    added field constants

---
+++ shop/constants.py
@@ -0,0 +1,18 @@
+CATEGORY = (
+	('Phones & Tablets', 'Phones & Tablets'),
+	('Electronics', 'Electronics'),
+	('Fashion', 'Fashion'),
+	('Computing', 'Computing'),
+	('Gaming', 'Gaming'),
+	('Home & Offices', 'Home & Offices'),
+	('Health & Beauty', 'Health & Beauty'),
+	('Supermarket', 'Supermarket'),
+	('Stay Safe', 'Stay Safe'),
+	('Library', 'Library'),
+	('Others', 'Others'),
+)
+
+STATUS = (
+	('Pending', 'Pending'),
+	('Delivered', 'Delivered'),
+)
